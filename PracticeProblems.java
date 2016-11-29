e363185b6155dc9b6a5f62a295450dc5032df0f1
Drew Beckmen <dbeckmen>
2016-11-29 17:55:09 -0500

    Fixed documentation

--- PracticeProblems.java
+++ PracticeProblems.java
@@ -1,9 +1,19 @@
+/**
+ * This class contains different methods that pertain to 2D arrays
+ * @author Drew Beckmen
+ * @version 1.0
+ */
 public class PracticeProblems
 {
+	/**
+	 * Main method that calls upon methods to build 2D arrays and print them
+	 * @param args Command line input
+	 */
 	public static void main(String[] args)
 	{
 		int[][] grid1 = buildSquare(); 
 		int [][] grid2 = buildSquare(8); 
+		
 		System.out.println("Here is the 4 x 4 array: ");
 		printArray(grid1); 
 		System.out.println("Here is the 8 x 8 array: ");
@@ -11,7 +21,11 @@ public class PracticeProblems
 		
 	}
 	
-	//Problem 1: 4 x 4 square
+	/**
+	 * Problem 1: 4 x 4 square
+	 * This method calls upon the other buildSquare method that can take in any dimensions and build the 2D array
+	 * @return A 2D int array that contains 4 arrays each with length 4
+	 */
 	public static int[][] buildSquare()
 	{
 		int[][] grid = buildSquare(4);
@@ -30,7 +44,13 @@ public class PracticeProblems
 		}*/
 	}
 	
-	//Problem 2 (overload) 
+	/**
+	 * Problem 2 (overload) 
+	 * This method has the same name as previous method but will be used if the parameter sideLength is passed in (overload)
+	 * Will create a 2D array that is built with dimensions n x n, where n is the inputted side length
+	 * @param sideLength The number of rows and columns in the 2D array
+	 * @return The 2D int array populated with values according to inputted dimensions. 
+	 */
 	public static int[][] buildSquare(int sideLength)
 	{
 		int[][] grid = new int[sideLength][sideLength];
@@ -46,7 +66,11 @@ public class PracticeProblems
 		return grid; 
 	}
 	
-	//Problem 3: Print it nicely
+	/**
+	 * Problem 3: Print it nicely
+	 * This method takes in a 2D int array and prints it out in a nicely formatted grid/chart
+	 * @param a The 2D int array that will be printed
+	 */
 	public static void printArray(int[][] a)
 	{ 
 		System.out.println(" " + helperMethod(a.length)); 
@@ -55,7 +79,7 @@ public class PracticeProblems
 			System.out.print("|"); 
 			for (int column = 0; column < a[row].length; column++)
 			{
-				//Test for length of item
+				//Test for length of item for formatting is correct
 				if (a[row][column] < 10)
 					System.out.print(a[row][column] + " |"); 
 				else
@@ -66,6 +90,11 @@ public class PracticeProblems
 		}	
 	}
 	
+	/**
+	 * A helper method to printArray that will create a string according to given length
+	 * @param length The number of times the characters have to be added to the string
+	 * @return A String that contains specific characters repeated number of times. 
+	 */
 	public static String helperMethod(int length)
 	{
 		String answer = new String(); 
